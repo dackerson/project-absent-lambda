dc738d22c4db88821ec2d7c16236fa1cac86d1bf
David Ackerson <l6qem@unb.ca>
2012-11-28 16:12:15 -0400

    Added stuff for different screens

--- src/myDisplay.cpp
+++ src/myDisplay.cpp
@@ -11,21 +11,31 @@ void myDisplay(void)
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);  // background is black
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    {
-        set<PointMass*>::iterator it;
-        for(it = rocks.begin(); it != rocks.end(); it++){
-            (*it)->Render();
-        }
-    }
+    switch(gameState){
+        case GAME_OVER_SCREEN:
+        case GAME_SCREEN:
+            {
+                set<PointMass*>::iterator it;
+                for(it = rocks.begin(); it != rocks.end(); it++){
+                    (*it)->Render();
+                }
+            }
 
-    {
-        set<LaserBeam*>::iterator it;
-        for(it = beams.begin(); it != beams.end(); it++){
-            (*it)->Render();
-        }
-    }
+            {
+                set<LaserBeam*>::iterator it;
+                for(it = beams.begin(); it != beams.end(); it++){
+                    (*it)->Render();
+                }
+            }
 
-    ship.Render();
+            ship.Render();
+
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10, 'A');
+
+            break;
+        case TITLE_SCREEN:
+            break;
+    }
 
     glFlush();
     glutSwapBuffers(); // display the screen just made
--- src/mySimulation.cpp
+++ src/mySimulation.cpp
@@ -8,146 +8,151 @@ void mySimulation(void){
 
     rockSpawnTimer += timeInterval;
 
-    if(rockSpawnTimer >= rockSpawnRate){
-        if(((int)(rocks.size())) == MAX_ROCKS){
-            rockSpawnTimer = rockSpawnRate;
-        }
-        else{
-            GLdouble size = 5.0; //((rand() % 5) + 5) / 1.0;
-            int startPosX = (rand() % (X_MAX - X_MIN)) + X_MIN;
-            int startPosY = (rand() % (Y_MAX - Y_MIN)) + Y_MIN;
-            int startSpeed = -10; //-(rand() % 40) - 10;
+    switch(gameState){
+        case GAME_SCREEN:
+            if(rockSpawnTimer >= rockSpawnRate){
+                if(((int)(rocks.size())) == MAX_ROCKS){
+                    rockSpawnTimer = rockSpawnRate;
+                }
+                else{
+                    GLdouble size = 5.0; //((rand() % 5) + 5) / 1.0;
+                    int startPosX = (rand() % (X_MAX - X_MIN)) + X_MIN;
+                    int startPosY = (rand() % (Y_MAX - Y_MIN)) + Y_MIN;
+                    int startSpeed = -10; //-(rand() % 40) - 10;
 
-            rockSpawnTimer = 0.0;
+                    rockSpawnTimer = 0.0;
 
-            PointMass* rock;
-            rock = new PointMass();
-			if(DEBUG == 1){
-				cout << "Creating Rock: " << ROCK_COUNT << endl;
-			}
-			rock->rockNumber = ROCK_COUNT++;
-            rock->Mass(1);
-            rock->BoundingSphereRadius(size);
+                    PointMass* rock;
+                    rock = new PointMass();
+                    if(DEBUG == 1){
+                        cout << "Creating Rock: " << ROCK_COUNT << endl;
+                    }
+                    rock->rockNumber = ROCK_COUNT++;
+                    rock->Mass(1);
+                    rock->BoundingSphereRadius(size);
 
-            // Set its starting location
-            initialLocation.set(Vector3(startPosX, startPosY, 250.0));
-            //initialLocation.set(Vector3(0.0, 0.0, 50.0));
-            rock->Location(initialLocation);
-            rock->LinearAcceleration(Vector3(0.0, 0.0, 0.0));
-            rock->LinearVelocity(Vector3(0.0, 0.0, startSpeed));
-            rock->Rotation(0.0);
-            rock->AngularVelocity(Vector3(1.00, 1.00, 1.00));
-            rocks.insert(rock);
-        }
-    }
-
-    //Little trick to make sure the scene is rendered
-    // without having to move the camera
-    if(start){
-		soundEngine.startBackgroundTrack();
-        cam.slide(0,0,-0.001);
-        glutPostRedisplay();
-        start = 0;
-    }
-
-    //Update the position of the rocks after timeInterval amount of time
-    {
-        set<PointMass*>::iterator it;
-        for(it = rocks.begin(); it != rocks.end(); it++){
-
-            (*it)->Update(timeInterval);
-
-			float rockCenterX = (float)((*it)->Location().x);
-			float rockCenterY = (float)((*it)->Location().y);
-			float rockCenterZ = (float)((*it)->Location().z);
-			float rockBoundingSphereRadius = (float)((*it)->BoundingSphereRadius());
-
-			float rockFront = (rockCenterZ) - (rockBoundingSphereRadius);
-			float rockBack = (rockCenterZ) + (rockBoundingSphereRadius);
-			float rockLeft = (rockCenterX) - (rockBoundingSphereRadius);
-			float rockRight = (rockCenterX) + (rockBoundingSphereRadius);
-			float rockTop = (rockCenterY) + (rockBoundingSphereRadius);
-			float rockBottom = (rockCenterY) - (rockBoundingSphereRadius);	
-				
-			float shipCenterX = (ship.Location().x);
-			float shipCenterY = (ship.Location().y);
-
-			float shipLeft = shipCenterX - (SHIP_WIDTH/2);
-			float shipRight = shipCenterX + (SHIP_WIDTH/2);
-			float shipTop = shipCenterY + (SHIP_HEIGHT);
-			float shipBottom = shipCenterY;
-
-			if((rockFront <= -SHIP_PLANE_Z + SHIP_LENGTH) && (rockBack > -SHIP_PLANE_Z)){
-				if((rockLeft < shipRight) && (rockRight > shipLeft) && (shipTop > rockBottom) && (shipBottom < rockTop)){
-					if(DEBUG == 1){
-						cout << "DAMAGE: Colision from Rock: " << (*it)->rockNumber << "!!!!!!!!" << endl;
-					}
-					soundEngine.playExplosionSound();
-					rocks.erase(it);
-				}
-			}
-
-            if((*it)->Location().z < 2){
-				if(DEBUG == 1){
-					cout << "Erasing Rock: " << (*it)->rockNumber << endl;
-				}
-                rocks.erase(it);
+                    // Set its starting location
+                    initialLocation.set(Vector3(startPosX, startPosY, 250.0));
+                    //initialLocation.set(Vector3(0.0, 0.0, 50.0));
+                    rock->Location(initialLocation);
+                    rock->LinearAcceleration(Vector3(0.0, 0.0, 0.0));
+                    rock->LinearVelocity(Vector3(0.0, 0.0, startSpeed));
+                    rock->Rotation(0.0);
+                    rock->AngularVelocity(Vector3(1.00, 1.00, 1.00));
+                    rocks.insert(rock);
+                }
             }
-        }
-    }
 
-    //Update the position of the beams after timeInterval amount of time
-    {
-        set<LaserBeam*>::iterator it;
-        for(it = beams.begin(); it != beams.end(); it++){
-            (*it)->Update(timeInterval);
-		
-			set<PointMass*>::iterator itRock;
-        	for(itRock = rocks.begin(); itRock != rocks.end(); itRock++){
-
-				float rockCenterX = (float)((*itRock)->Location().x);
-				float rockCenterY = (float)((*itRock)->Location().y);
-				float rockCenterZ = (float)((*itRock)->Location().z);
-				float rockBoundingSphereRadius = (float)((*itRock)->BoundingSphereRadius());
-
-				float rockFront = (rockCenterZ) - (rockBoundingSphereRadius);
-				float rockBack = (rockCenterZ) + (rockBoundingSphereRadius);
-				float rockLeft = (rockCenterX) - (rockBoundingSphereRadius);
-				float rockRight = (rockCenterX) + (rockBoundingSphereRadius);
-				float rockTop = (rockCenterY) + (rockBoundingSphereRadius);
-				float rockBottom = (rockCenterY) - (rockBoundingSphereRadius);
-
-				float laserCenterX = ((*it)->Location().x);
-				float laserCenterY = ((*it)->Location().y);
-				float laserCenterZ = ((*it)->Location().z);
-				float laserRadius = ((*it)->laserRadius);
-
-				float laserFront = (laserCenterZ) - (laserRadius);
-				float laserBack = (laserCenterZ);
-				float laserLeft = (laserCenterX) - (laserRadius);
-				float laserRight = (laserCenterX) + (laserRadius);
-				float laserTop = (laserCenterY) + (laserRadius);
-				float laserBottom = (laserCenterY) - (laserRadius);
-
-				if((laserBack > rockFront) && (laserFront < rockBack)){
-					if((rockLeft < laserRight) && (rockRight > laserLeft) && (laserTop > rockBottom) && (laserBottom < rockTop)){
-						if(DEBUG == 1){
-							cout << "Laser: " << (*it)->laserBeamNumber << " collides with rock: " << (*itRock)->rockNumber << endl;
-						}
-						soundEngine.playExplosionSound();
-						rocks.erase(itRock);
-						beams.erase(it);
-					}
-				}
-			}
-
-            if(((*it)->Location().minus((*it)->length)).z > FAR_PLANE){
-				if(DEBUG == 1){
-					cout << "Erasing Laser Beam: "<< (*it)->laserBeamNumber << endl;
-				}
-                beams.erase(it);
+            //Little trick to make sure the scene is rendered
+            // without having to move the camera
+            if(start){
+                soundEngine.startBackgroundTrack();
+                cam.slide(0,0,-0.001);
+                glutPostRedisplay();
+                start = 0;
             }
-        }
+
+            //Update the position of the rocks after timeInterval amount of time
+            {
+                set<PointMass*>::iterator it;
+                for(it = rocks.begin(); it != rocks.end(); it++){
+
+                    (*it)->Update(timeInterval);
+
+                    float rockCenterX = (float)((*it)->Location().x);
+                    float rockCenterY = (float)((*it)->Location().y);
+                    float rockCenterZ = (float)((*it)->Location().z);
+                    float rockBoundingSphereRadius = (float)((*it)->BoundingSphereRadius());
+
+                    float rockFront = (rockCenterZ) - (rockBoundingSphereRadius);
+                    float rockBack = (rockCenterZ) + (rockBoundingSphereRadius);
+                    float rockLeft = (rockCenterX) - (rockBoundingSphereRadius);
+                    float rockRight = (rockCenterX) + (rockBoundingSphereRadius);
+                    float rockTop = (rockCenterY) + (rockBoundingSphereRadius);
+                    float rockBottom = (rockCenterY) - (rockBoundingSphereRadius);	
+                        
+                    float shipCenterX = (ship.Location().x);
+                    float shipCenterY = (ship.Location().y);
+
+                    float shipLeft = shipCenterX - (SHIP_WIDTH/2);
+                    float shipRight = shipCenterX + (SHIP_WIDTH/2);
+                    float shipTop = shipCenterY + (SHIP_HEIGHT);
+                    float shipBottom = shipCenterY;
+
+                    if((rockFront <= -SHIP_PLANE_Z + SHIP_LENGTH) && (rockBack > -SHIP_PLANE_Z)){
+                        if((rockLeft < shipRight) && (rockRight > shipLeft) && (shipTop > rockBottom) && (shipBottom < rockTop)){
+                            if(DEBUG == 1){
+                                cout << "DAMAGE: Colision from Rock: " << (*it)->rockNumber << "!!!!!!!!" << endl;
+                            }
+                            soundEngine.playExplosionSound();
+                            rocks.erase(it);
+                        }
+                    }
+
+                    if((*it)->Location().z < 2){
+                        if(DEBUG == 1){
+                            cout << "Erasing Rock: " << (*it)->rockNumber << endl;
+                        }
+                        rocks.erase(it);
+                    }
+                }
+            }
+
+            //Update the position of the beams after timeInterval amount of time
+            {
+                set<LaserBeam*>::iterator it;
+                for(it = beams.begin(); it != beams.end(); it++){
+                    (*it)->Update(timeInterval);
+                
+                    set<PointMass*>::iterator itRock;
+                    for(itRock = rocks.begin(); itRock != rocks.end(); itRock++){
+
+                        float rockCenterX = (float)((*itRock)->Location().x);
+                        float rockCenterY = (float)((*itRock)->Location().y);
+                        float rockCenterZ = (float)((*itRock)->Location().z);
+                        float rockBoundingSphereRadius = (float)((*itRock)->BoundingSphereRadius());
+
+                        float rockFront = (rockCenterZ) - (rockBoundingSphereRadius);
+                        float rockBack = (rockCenterZ) + (rockBoundingSphereRadius);
+                        float rockLeft = (rockCenterX) - (rockBoundingSphereRadius);
+                        float rockRight = (rockCenterX) + (rockBoundingSphereRadius);
+                        float rockTop = (rockCenterY) + (rockBoundingSphereRadius);
+                        float rockBottom = (rockCenterY) - (rockBoundingSphereRadius);
+
+                        float laserCenterX = ((*it)->Location().x);
+                        float laserCenterY = ((*it)->Location().y);
+                        float laserCenterZ = ((*it)->Location().z);
+                        float laserRadius = ((*it)->laserRadius);
+
+                        float laserFront = (laserCenterZ) - (laserRadius);
+                        float laserBack = (laserCenterZ);
+                        float laserLeft = (laserCenterX) - (laserRadius);
+                        float laserRight = (laserCenterX) + (laserRadius);
+                        float laserTop = (laserCenterY) + (laserRadius);
+                        float laserBottom = (laserCenterY) - (laserRadius);
+
+                        if((laserBack > rockFront) && (laserFront < rockBack)){
+                            if((rockLeft < laserRight) && (rockRight > laserLeft) && (laserTop > rockBottom) && (laserBottom < rockTop)){
+                                if(DEBUG == 1){
+                                    cout << "Laser: " << (*it)->laserBeamNumber << " collides with rock: " << (*itRock)->rockNumber << endl;
+                                }
+                                soundEngine.playExplosionSound();
+                                rocks.erase(itRock);
+                                beams.erase(it);
+                            }
+                        }
+                    }
+
+                    if(((*it)->Location().minus((*it)->length)).z > FAR_PLANE){
+                        if(DEBUG == 1){
+                            cout << "Erasing Laser Beam: "<< (*it)->laserBeamNumber << endl;
+                        }
+                        beams.erase(it);
+                    }
+                }
+            }
+
+            break;
     }
 
     //Call to myDisplay to render the updated scene
