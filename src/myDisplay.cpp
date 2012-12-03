5aa0e9ad6c816d8b57152d26127630fe6bd181c0
David Ackerson <david.a912@gmail.com>
2012-12-03 08:19:40 -0400

    Text now appears

--- src/myDisplay.cpp
+++ src/myDisplay.cpp
@@ -11,6 +11,11 @@ void myDisplay(void)
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);  // background is black
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    Point3 shipLocation = ship.Location();
+    Point3 eye(shipLocation.x, shipLocation.y, 0.0);
+    Point3 look(shipLocation.x, shipLocation.y, 1.0);
+    Vector3 up(0.0, 1.0, 0.0);
+
     switch(gameState){
         case GAME_OVER_SCREEN:
         case GAME_SCREEN:
@@ -30,8 +35,34 @@ void myDisplay(void)
 
             ship.Render();
 
-            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10, 'A');
+            glDisable(GL_TEXTURE_2D);
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            glOrtho(0, 1, 0, 1, 0, 1);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
 
+            glColor4f(0.0, 1.0, 0.0, 0.0);
+            glRasterPos2d(0.0, 0.95);
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, 'A');
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, 'B');
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, 'C');
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, 'D');
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, 'E');
+
+            glRasterPos2d(0.0, 0.9);
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, '=');
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, '=');
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, '=');
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, '=');
+
+            //glMatrixMode(GL_MODELVIEW);
+            //glLoadIdentity();
+            
+            cam.setShape(VIEW_ANGLE, (WINDOW_WIDTH * 1.0) / WINDOW_HEIGHT, NEAR_PLANE, FAR_PLANE);
+            cam.set(eye, look, up);
+            glEnable(GL_TEXTURE_2D);
+            
             break;
         case TITLE_SCREEN:
             break;
