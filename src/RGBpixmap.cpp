e6f5353d711b7c0f981711ef240cdb5ef52ca662
David Ackerson <l6qem@unb.ca>
2012-12-03 16:11:45 -0400

    Rolled up image loading in initTexture into a loop

--- src/RGBpixmap.cpp
+++ src/RGBpixmap.cpp
@@ -18,51 +18,25 @@ void initTexture(void)
         gluQuadricNormals(qobj, GLU_SMOOTH);
         gluQuadricTexture(qobj, GL_TRUE);
 
-        string s0 = "../textures/shiphull.bmp";
-	int ret = pix[0].readBMPFile(s0);
-        pix[0].setTexture(2001);
-        
-        string s1 = "../textures/logoNose.bmp";
-	ret = pix[1].readBMPFile(s1);
-        pix[1].setTexture(2002);
-        
-        string s2 = "../textures/logoTail.bmp";
-	ret = pix[2].readBMPFile(s2);
-        pix[2].setTexture(2003);
-        
-        string s3 = "../textures/barrel.bmp";  
-	ret = pix[3].readBMPFile(s3);
-        pix[3].setTexture(2004);
-          
-        string s5 = "../textures/gunmouth.bmp";//laser gun tip
-	ret = pix[4].readBMPFile(s5);
-        pix[4].setTexture(2005);
-        
-        string s4 = "../textures/shipengine.bmp";
-	ret = pix[5].readBMPFile(s4);
-        pix[5].setTexture(2006);   
+    string images[] = {
+        "../textures/shiphull.bmp",
+        "../textures/logoTail.bmp",
+        "../textures/logoTail.bmp",
+        "../textures/gunmouth.bmp",
+        "../textures/shipengine.bmp",
+        "../textures/shipengine.bmp",
+        "../textures/jup2vuu2.bmp",
+        "../textures/mar1kuu2.bmp",
+        "../textures/mar2kuu2.bmp",
+        "../textures/plu0rss1.bmp",
+        "../textures/plu1rss1.bmp",
+        "../textures/sky.bmp",
+        "\0"
+    };
 
-        string s10 = "../textures/jup2vuu2.bmp";
-	ret = pix[6].readBMPFile(s10);
-        pix[6].setTexture(2007);
-        
-        string s6 = "../textures/mar1kuu2.bmp";
-	ret = pix[7].readBMPFile(s6);
-        pix[7].setTexture(2008);
-        
-        string s7 = "../textures/mar2kuu2.bmp";  
-	ret = pix[8].readBMPFile(s7);
-        pix[8].setTexture(2009);
-          
-        string s8 = "../textures/plu0rss1.bmp";//laser gun tip
-	ret = pix[9].readBMPFile(s8);
-        pix[9].setTexture(2010);
-        
-        string s9 = "../textures/plu1rss1.bmp";
-	ret = pix[10].readBMPFile(s9);
-        pix[10].setTexture(2011);  
-        
-        string s11 = "../textures/sky.bmp";//star map for background
-	ret = pix[11].readBMPFile(s11);
-        pix[11].setTexture(2012);   
-}
+    for(int i = 0; images[i][0] != '\0'; i++){
+        pix[i].readBMPFile(images[i]);
+        pix[i].setTexture(2001 + i);
+    }
+
+ }
