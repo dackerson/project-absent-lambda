92c55842b93513b5c7f6b300b4909c20a431743c
Graham <f8j31@id415m31.cs.unb.ca>
2012-12-05 20:26:58 -0400

    code cleanup / Commenting

--- src/RGBpixmap.cpp
+++ src/RGBpixmap.cpp
@@ -18,29 +18,31 @@ void initTexture(void)
         gluQuadricNormals(qobj, GLU_SMOOTH);
         gluQuadricTexture(qobj, GL_TRUE);
 
+	//File names of textures to import
     string images[] = {
-        "../textures/shiphull.bmp",
+        "../textures/shiphull.bmp",//2001
         "../textures/logoNose.bmp",
         "../textures/logoTail.bmp",
         "../textures/barrel.bmp",
-        "../textures/gunmouth.bmp",
+        "../textures/gunmouth.bmp",//2005
         "../textures/shipengine.bmp",
         "../textures/jup2vuu2.bmp",
         "../textures/mar1kuu2.bmp",
         "../textures/mar2kuu2.bmp",
-        "../textures/plu0rss1.bmp",
+        "../textures/plu0rss1.bmp",//2010
         "../textures/plu1rss1.bmp",
         "../textures/sky.bmp",
         "../textures/earth1.bmp",
         "../textures/moonmercator.bmp",
         "../textures/gameOver.bmp",
-        "../textures/bluescreen.bmp",
-        "\0"
+        "../textures/bluescreen.bmp",//2015
+		//Add New textures here...
+        "\0"//No more textures
     };
 
     for(int i = 0; images[i][0] != '\0'; i++){
-        pix[i].readBMPFile(images[i]);
-        pix[i].setTexture(2001 + i);
+        pix[i].readBMPFile(images[i]);//Read in texture
+        pix[i].setTexture(2001 + i);//Set it to the appropriate value
     }
 
  }
